96b7227303814b9e2e9714b8fbbbbfb4a08126f2
jermz678 <jermz678@live.com>
2021-03-29 13:38:48 -0500

    ux enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -21,6 +21,7 @@ var auditTask = function(taskEl) {
     else if (Math.abs(moment().diff(time, "days")) <= 2) {
       $(taskEl).addClass("list-group-item-warning");
     }
+    console.log(taskEl)
 };
 
 
@@ -56,39 +57,32 @@ var loadTasks = function() {
     };
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   $(".card .list-group").sortable({
     connectWith: $(".card .list-group"),
     scroll: false,
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-      console.log("activate", this);
+     console.log("activate", this);
+      $(this).addClass("dropover");
+      $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function(event) {
       console.log("deactivate", this);
+      $(this).removeClass("dropover");
+      $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event) {
       console.log("over", event.target);
+      $(this).addClass("dropover-active");
+      $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event) {
       console.log("out", event.target);
-    },
+      $(this).removeClass("dropover-active");
+      $(".bottom-trash").removeClass("bottom-trash-active");
+   },
+   
     update: function(event) {
       var tempArr = [];
       // loop over current set of children in sortable list
@@ -150,7 +144,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -311,4 +305,10 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+    
+  });
+  
+}, (1000 * 60) * 30);
